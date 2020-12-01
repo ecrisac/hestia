e61c62e4107a72661c07dcb7fe1f681efdfa1b7b
Eugen Crisac <ecrisac@mixbook.com>
2020-12-01 23:00:12 +0200

    Update to support rails 4

--- lib/hestia/railtie.rb
+++ lib/hestia/railtie.rb
@@ -1,4 +1,6 @@
-require "hestia"
+# frozen_string_literal: true
+
+require 'hestia'
 
 module Hestia
   class Railtie < ::Rails::Railtie
@@ -6,10 +8,13 @@ module Hestia
     #
     # See README.md for how to configure this in your application.
     #
-    initializer "hestia.signed_cookie_jar_extension", before: :load_config_initializers do
+    initializer 'hestia.signed_cookie_jar_extension', before: :load_config_initializers do
       extension = case ActionPack::VERSION::MAJOR
-      when 3
-        Hestia::SignedCookieJarExtension::ActionPack3
+                  when 3
+                    Hestia::SignedCookieJarExtension::ActionPack3
+                  when 4
+                    Hestia.check_secret_key_base
+                    Hestia::SignedCookieJarExtension::ActionPack4
       end
 
       ActionDispatch::Cookies::SignedCookieJar.prepend(extension)
--- lib/hestia/signed_cookie_jar_extension.rb
+++ lib/hestia/signed_cookie_jar_extension.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 module Hestia
   module SignedCookieJarExtension
-    autoload :ActionPack3, "hestia/signed_cookie_jar_extension/action_pack_3"
+    autoload :ActionPack3, 'hestia/signed_cookie_jar_extension/action_pack_3'
+    autoload :ActionPack4, 'hestia/signed_cookie_jar_extension/action_pack_4'
   end
 end
