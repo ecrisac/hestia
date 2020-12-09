15a29cd0741be7a2b613d38ec37591cf458221c1
Eugen Crisac <ecrisac@mixbook.com>
2020-12-09 16:30:05 +0200

    Add safe json serializer

--- lib/hestia/signed_cookie_jar_extension/action_pack_4.rb
+++ lib/hestia/signed_cookie_jar_extension/action_pack_4.rb
@@ -5,6 +5,8 @@ require 'active_support/message_encryptor'
 module Hestia
   module SignedCookieJarExtension
     module ActionPack4
+      require 'safe_json_serializer'
+
       # Public: overridden #initialize method
       #
       # In rails, `secrets' will be given the value of `Rails.application.config.secret_token'. That's the current secret token.
@@ -17,12 +19,24 @@ module Hestia
       def initialize(parent_jar, key_generator, options = {})
         super
 
+        options = if Rails.application.config.respond_to?(:options)
+                    Rails.application.config.options
+                  else
+                    {}
+        end
+
         # Find the deprecated secrets, if there are any
         deprecated_secrets = if Rails.application.config.respond_to?(:deprecated_secret_token)
                                # This could be a single string!
                                Array(Rails.application.config.deprecated_secret_token)
                              else
                                []
+        end
+
+        deprecated_options = if Rails.application.config.respond_to?(:deprecated_options)
+                               Array(Rails.application.config.deprecated_options)
+                             else
+                               [{}]
         end
 
         # Grab the `config.secret_token` value from its generator
@@ -36,7 +50,12 @@ module Hestia
         serializer = ActiveSupport::MessageEncryptor::NullSerializer
 
         # Finally, override @verifier with our own multi verifier containing all the secrets
-        @verifier = Hestia::MessageMultiVerifier.new(current_secret: active_secret, deprecated_secrets: deprecated_secrets, options: { serializer: serializer })
+        @verifier = Hestia::MessageMultiVerifier.new(
+          current_secret: secret,
+          options: options,
+          deprecated_secrets: deprecated_secrets,
+          deprecated_options: deprecated_options
+        )
       end
     end
   end
